fd05c236ac83709586193dcb349e94b9bbb4d858
yonghyunjay <tnsend48@naver.com>
2020-05-14 11:22:59 +0900

    Add templates files

--- SemiProj3/movieApp/views.py
+++ SemiProj3/movieApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+ 
+def index(request):
+    template = loader.get_template('movieApp/index.html')
+    context = {
+        'latest_question_list': "test",
+    }
+    return HttpResponse(template.render(context, request))
