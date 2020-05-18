8c9e3443eb3260749522f67e25631602bc17827e
mirae <zzomill0427@gmail.com>
2020-05-19 08:53:45 +0900

    mirae

--- model/mirae/knn_recommender.py
+++ model/mirae/knn_recommender.py
@@ -1,10 +1,12 @@
+
 # usage :: 'python model/mirae/knn_recommender.py --movie_name "Iron Man" --top_n 10' # 팀 폴더 
 # usage :: 'python knn_recommender.py --movie_name "Iron Man" --top_n 10' #개인 폴더
 import os
 import time
 import gc
 import argparse
-
+# import total
+import numpy as np
 # data science imports
 import pandas as pd
 from scipy.sparse import csr_matrix
@@ -13,8 +15,6 @@ from sklearn.neighbors import NearestNeighbors
 # utils import
 from fuzzywuzzy import fuzz
 
-total_title = []
-total_dist = []
 
 
 class KnnRecommender:
@@ -63,7 +63,7 @@ class KnnRecommender:
         """
         prepare data for recommender
         1. movie-user scipy sparse matrix
-        2. hashmap of movie to row index in movie-user scipy sparse matrix
+        2. hashmap of movie to row index in movie-user scipy sparse matrix #영화와 인덱스간의 해쉬맵
         """
         # read data
         df_movies = pd.read_csv(
@@ -78,7 +78,7 @@ class KnnRecommender:
         df_movies_cnt = pd.DataFrame(
             df_ratings.groupby('movieId').size(),
             columns=['count'])
-        popular_movies = list(set(df_movies_cnt.query('count >= @self.movie_rating_thres').index))  # noqa
+        popular_movies = list(set(df_movies_cnt.query('count >= @self.movie_rating_thres').index))  # 인기영화위주로 
         movies_filter = df_ratings.movieId.isin(popular_movies).values
 
         df_users_cnt = pd.DataFrame(
@@ -94,8 +94,7 @@ class KnnRecommender:
             index='movieId', columns='userId', values='rating').fillna(0)
         # create mapper from movie title to index
         hashmap = {
-            movie: i for i, movie in
-            enumerate(list(df_movies.set_index('movieId').loc[movie_user_mat.index].title)) # noqa
+            movie: i for i, movie in enumerate(list(df_movies.set_index('movieId').loc[movie_user_mat.index].title)) # noqa
         }
         # transform matrix to scipy sparse matrix
         movie_user_mat_sparse = csr_matrix(movie_user_mat.values)
@@ -148,11 +147,12 @@ class KnnRecommender:
         ------
         list of top n similar movie recommendations
         """
-        # fit
+        # fit : 전처리한 데이터 학습
         model.fit(data)
         # get input movie index
         print('You have input movie:', fav_movie)
-        idx = self._fuzzy_matching(hashmap, fav_movie)
+        ## 입력받은 여기서 idx를 2개 받는다면..
+        idx = self._fuzzy_matching(hashmap, fav_movie) # 입력된 movie와 맵핑된 index를 찾는다. 
         # inference
         print('Recommendation system start to make inference')
         print('......\n')
@@ -160,16 +160,9 @@ class KnnRecommender:
         distances, indices = model.kneighbors(
             data[idx],
             n_neighbors=n_recommendations+1)
+
         # get list of raw idx of recommendations
-        raw_recommends = \
-            sorted(
-                list(
-                    zip(
-                        indices.squeeze().tolist(),
-                        distances.squeeze().tolist()
-                    )
-                ),
-                key=lambda x: x[1]
+        raw_recommends = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1]
             )[:0:-1]
         print('It took my system {:.2f}s to make inference \n\
               '.format(time.time() - t0))
@@ -184,31 +177,35 @@ class KnnRecommender:
         fav_movie: str, name of user input movie
         n_recommendations: int, top n recommendations
         """
-        # get data
+        # get data : 전처리 & 입력데이터 
         movie_user_mat_sparse, hashmap = self._prep_data()
         # get recommendations
-        raw_recommends = self._inference(
-            self.model, movie_user_mat_sparse, hashmap,
+        raw_recommends = self._inference(self.model, movie_user_mat_sparse, hashmap,
             fav_movie, n_recommendations)
+        print('n_recommendations:', n_recommendations)
         # print results
         reverse_hashmap = {v: k for k, v in hashmap.items()}
-        print('Recommendations for {}:'.format(fav_movie))
+        # print('Recommendations for {}:'.format(fav_movie))
 
         # 제목(ran_title), 유사도 담기 
-        ran_title = []
-        ran_dist = [] # 유사도 
+        total_title_=[]
         for i, (idx, dist) in enumerate(raw_recommends):
             # print('{0}: {1}'.format(i+1, reverse_hashmap[idx]))
-            ran_title.append(reverse_hashmap[idx])
-            ran_dist.append(dist)
+            total_title_.append(reverse_hashmap[idx])
 
-        for rt in range(0, len(ran_title)):
-            # print('ran_title: ', ran_title[rt])
-            # print('ran_dist[rt]: ', ran_dist[rt])
-            total_title.append(ran_title[rt])
-            # total_dist.append(ran_dist[rt])
-            # print('{0}: {1}, with distance '
-            #       'of {2}'.format(i+1, reverse_hashmap[idx], dist))
+        # print('total_title_::', total_title_)
+        return total_title_
+            
+        #     ran_title.append(reverse_hashmap[idx])
+        #     ran_dist.append(dist)
+
+        # for rt in range(0, len(ran_title)):
+        #     print('ran_title: ', ran_title[rt])
+        #     # print('ran_dist[rt]: ', ran_dist[rt])
+        #     return ran_title[rt]
+        #     # total_dist.append(ran_dist[rt])
+        #     print('{0}: {1}, with distance .'
+        #           'of {2}'.format(i+1, reverse_hashmap[idx], dist))
 # 1: Kung Fu Panda (2008), with distance of 0.37368708848953247
 # 2: Inception (2010), with distance of 0.3691744804382324
 
@@ -232,45 +229,76 @@ def parse_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+#movie_name_ : 입력값
+def main(movie_name_):
+# if __name__ == '__main__':
     # get args
+    # 문자열 그대로 가져감
     args = parse_args()
-    # movie_name2 = args.movie_name.split(',')
-    #################사용자 입력 값 받는 부분 
-    movie_name_ = 'superman, notting hills, interstella, the brave heart'
-    movie_name2 = movie_name_.split(',')
-    ratings_filename = args.ratings_filename
-    data_path = args.path
-    movies_filename = args.movies_filename
-    top_n = args.top_n
+    # movie_name2 : 사용자 값이 들어감(복수개)
+    movie_name_ = 'superman, nottinghills, avatar'
+    movie_name2 = movie_name_.split(',') # 자체가 배열 
+    # 나머지 입력값
+    ratings_filename = args.ratings_filename #ratings.csv
+    data_path = args.path # default path 찾아감
+    movies_filename = args.movies_filename # movies.csv - str그대로다. 
+    top_n = args.top_n 
+    total_title = []
+    ran = [] 
+    # 입력값 차례로 knnmodel에 들어감
     for i in range(0, len(movie_name2)):
         movie_name = movie_name2[i] 
-        # initial recommender system
+        # 1. initial recommender system
         recommender = KnnRecommender(
             os.path.join(data_path, movies_filename),
             os.path.join(data_path, ratings_filename))
-        # set params
-        recommender.set_filter_params(50, 50)
-        recommender.set_model_params(20, 'brute', 'cosine', -1)
-        # make recommendations
-        recommender.make_recommendations(movie_name, top_n)
+       # <__main__.KnnRecommender object at 0x1a17f2d350>
 
+        # 2. set params
+        recommender.set_filter_params(50, 50) #user, movies의 안좋은 데이터 버림
+        recommender.set_model_params(20, 'brute', 'cosine', -1) #  n_neighbors, algorithm, metric, n_jobs=None
 
-#파일 저장
-import csv
-import random
-for i in range(0, 3):
+        # 3. make recommendations
+        total_title.append(recommender.make_recommendations(movie_name, top_n))
+    # 확인
+    # total_title = np.array(total_title)
+    total_title = np.array(total_title)
+    num = total_title.shape[0]*total_title.shape[1]
+    total_title = total_title.reshape(num)
+    
+    import random
+    # 랜덤 10개
     random.shuffle(total_title)
+    for i in range(0, 10):
+        ran.append(total_title[i])
+    print('ran::', ran)
+    # 함수 
+    return ran
+    
+    # totla_title_fin = []
+    # for i range(0, )
+    
+    
+    # print('total_title:::', total_title)
 
-rows = []
-for i in range(0, 10):
-    title = total_title[i]
-    rows.append({'num':i+1, 'title':title}) #dict형태
-    print(f'result : {i+1} {title}')
+# import csv
 
-with open("./recommendations.csv", 'w') as f :
-    fieldnames = ['num','title']
-    writer = csv.DictWriter(f, fieldnames = fieldnames)
-    writer.writeheader()
-    writer.writerows(rows)
-f.close()
+# import random
+# for i in range(0, 3):
+#     random.shuffle(total_title)
+# rows = []
+# for i in range(0, 10):
+#     title = total_title[i]
+#     rows.append({'num':i+1, 'title':title}) #dict형태
+#     print(f'result : {i+1} {title}')
+
+# with open("./recommendations.csv", 'w') as f :
+#     fieldnames = ['num','title']
+#     writer = csv.DictWriter(f, fieldnames = fieldnames)
+#     writer.writeheader()
+#     writer.writerows(rows)
+
+
+# f.close()
+
+
